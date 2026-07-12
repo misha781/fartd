94d45369fd9dfe615ece5a9c824476e8b87f2dbc
misha781 <mishkolz53@gmail.com>
2026-07-12 07:57:40 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 echo InstallerScript
-
-mv fartd.service /etc/systemd/system/fartd.service
+sed -i "s/REPLACE_ME/$SUDO_USER/g" /home/$SUDO_USER/.fartd/fartd.service
+mv /home/$SUDO_USER/.fartd/fartd.service /etc/systemd/system/fartd.service
 
 systemctl daemon-reload
-sudo systemctl enable --now fartd.service
+systemctl enable --now fartd.service
